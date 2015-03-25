56371e1c486bd3a4eb78e71e3a5ab91e6f13c86d
camilleriluke <mail@camilleriluke.com>
2015-03-25 22:48:36 +0100

    commit

--- src/test/java/mt/edu/um/luke/AppTest.java
+++ src/test/java/mt/edu/um/luke/AppTest.java
@@ -1,9 +1,16 @@
 package mt.edu.um.luke;
 
+import org.junit.Test;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
+
+    @Test
+    public void testThis() {
+        System.out.println("hello");
+    }
 }
 

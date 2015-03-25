a7168b832da65fa7393d8d89b0568ef2b610a3df
camilleriluke <mail@camilleriluke.com>
2015-03-25 22:35:53 +0100

    initial commit

---
+++ src/main/java/mt/edu/um/luke/App.java
@@ -0,0 +1,13 @@
+package mt.edu.um.luke;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
